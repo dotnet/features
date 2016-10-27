fb929c9877d12223b3f8f1057e580f5c6f1b3e30
Richard Lander <rlander@microsoft.com>
2016-10-27 10:10:08 -0700

    Stopping point

--- console/NETCatalog.CLI/InteractiveCatalog.cs
+++ console/NETCatalog.CLI/InteractiveCatalog.cs
@@ -11,27 +11,78 @@ public class InteractiveCatalog
 
     public static async Task Start()
     {
-        string line = String.Empty;
+        string response = null;
         string category = null;
+        string[] state = null;
+
+        while (true)
+        {
+
+        }
 
         do
         {
-            Console.Clear();
-
-            if (category == null)
+            if (response == null && state == null)
+            {}
+            else if (response == String.Empty && state == null)
             {
-
+                return;
             }
+            else if (response != string.Empty && state == null)
+            {
+                category = response;
+                response = null;
+            }
+            else if (response == string.Empty && state[0] == "category")
+            {
+                category = null;
+                response = null;
+            }
+            else if (response == string.Empty && state[0] == "concept")
+            {
+                category = state[1];
+                response = null;
+            }
+            else if (state[0] == "category")
+            {
+                category = state[1];
+            }
+                
+            state = await GetCatalogEntry(category, response);
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Type value or press [Enter] to exit.");
+            Console.WriteLine();
 
-            category = await GetCatalogEntry(category, line);
-        } while ((line = Console.ReadLine()) != null);
+            Console.Write("> ");
+        } while ((response = Console.ReadLine()) != null);
 
     }
 
-    private static async Task<string> GetCatalogEntry(string category, string concept)
+    private static async Task PrintConceptsForCategory(string category)
+    {
+        string catalogEntry = null;
+            
+        catalogEntry = await _client.GetStringAsync($"{_url}/{category}");
+        var concepts = SplitLines(catalogEntry);
+
+        Console.WriteLine($"Concepts under /{category}/ you can learn more about:\n");
+        foreach (var c in concepts)
+        {
+            Console.WriteLine($"\t{c}");
+        }
+    }
+
+    private static async Task PrintConceptForCategory(string category, string concept)
+    {
+        var catalogEntry = await _client.GetStringAsync($"{_url}/{category}/{concept}");
+        Console.WriteLine(catalogEntry);
+    }
+
+    private static async string[] GetCatalogRequest(string category, string concept)
     {
 
-        if (category == null)
+        if (string.IsNullOrEmpty(category))
         {
             var catalogEntry = await _client.GetStringAsync(_url);
             var categories = SplitLines(catalogEntry);
@@ -43,9 +94,19 @@ public class InteractiveCatalog
             }
             return null;
         }
-        else if (concept == null)
+        else if (string.IsNullOrEmpty(concept))
         {
-            var catalogEntry = await _client.GetStringAsync($"{_url}/{category}");
+            string catalogEntry = null;
+            
+            try
+            {
+                catalogEntry = await _client.GetStringAsync($"{_url}/{category}");
+            }
+            catch
+            {
+                Console.WriteLine($"Not a valid category: {category}");
+                return new[] {"invalid"};
+            }
             var concepts = SplitLines(catalogEntry);
 
             Console.WriteLine($"Concepts under /{category}/ you can learn more about:\n");
@@ -53,13 +114,13 @@ public class InteractiveCatalog
             {
                 Console.WriteLine($"\t{c}");
             }
-            return category;
+            return new[] {"category",category};
         }
         else 
         {
             var catalogEntry = await _client.GetStringAsync($"{_url}/{category}/{concept}");
             Console.WriteLine(catalogEntry);
-            return category;
+            return  new[] {"concept",category,concept};
         }
     }
 
